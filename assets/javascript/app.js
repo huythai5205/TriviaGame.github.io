4be36954caa02c730657b8a0aad147ea63c852d6
Huy <kennyt995@gmail.com>
2017-12-06 13:31:46 -0700

    stuck on settimeout function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,13 @@
 $(document).ready(function () {
 
+    var timer;
+    var score = 0;
+
     $('#startGame').click(function () {
         pickQuestion();
     });
 
-    function renderCheckBox(question) {
-        $('.game').empty();
-        $('.game').html(`<h2>Question: 1</h2>
-  <p>${question.question}</p>
-  <div class="choices-container">
-  <form>
-    <div class="checkbox">
-      <label><input type="checkbox" value="${question.choice1}">${question.choice1}</label>
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" value="${question.choice2}">${question.choice2}</label>
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" value="${question.choice3}">${question.choice3}</label>
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" value="${question.choice4}">${question.choice4}</label>
-    </div>
-    <button id="submit">Submit</button>
-    </form>
-</div>`);
-    }
-
-    function renderRadioButton(question) {
+    function renderTF(question) {
         $('.game').empty();
         $('.game').html(`<h2>Question: 1</h2>
   <p>${question.question}</p>
@@ -40,33 +20,81 @@ $(document).ready(function () {
 <div class="radio">
   <label><input type="radio" name="optradio" value="${question.choice2}">${question.choice2}</label>
 </div>
+    <button id="submit" value="${question.answer}>Submit</button>
+    </form>
+</div>`);
+        timer = setTimeout(() => {
+            renderTimesup
+        }, 100);
+    }
+
+    function renderRadioButton(question) {
+        $('.game').empty();
+        $('.game').html(`<h2>Question: 1</h2>
+  <p>${question.question}</p>
+  </div>
+  <div class="choices-container">
+  <form>
+  <div class="radio">
+  <label><input type="radio" name="optradio" value="${question.choice1}">${question.choice1}</label>
+    </div>
+    <div class="radio">
+  <label><input type="radio" name="optradio" value="${question.choice2}">${question.choice2}</label>
+</div>
 <div class="radio">
   <label><input type="radio" name="optradio" value="${question.choice3}">${question.choice3}</label>
 </div>
 <div class="radio">
   <label><input type="radio" name="optradio" value="${question.choice4}">${question.choice4}</label>
 </div>
-<button id="submit">Submit</button>
+<button id="submit" value="${question.answer}">Submit</button>
 </form>
 </div>`);
+        timer = setTimeout(() => {
+            renderTimesup
+        }, 100);
+    }
+
+    function renderTimesup() {
+        $('.game').empty();
+        $('.game').html(`<h1>Time's up</h1>`);
+    }
+
+    function renderAnswerWrong() {
+        $('.game').empty();
+        $('.game').html(`<h1>Answer is incorrect</h1>`);
+    }
+
+    function renderAnswerRight() {
+        $('.game').empty();
+        $('.game').html(`<h1>Answer is correct</h1>`);
     }
 
     $(document).on("click", '#submit', function () {
-        var array = [];
+        clearTimeout(timer);
         var value = $('input:checked').val();
-        $("input:checked").each(function () {
-            array.push($(this).val());
-        });
-        alert(array);
+        var answer = $('#submit').val();
+        if (value === answer) {
+            score++;
+            setTimeout(() => {
+                renderAnswerRight
+            }, 500);
+        } else {
+            setTimeout(() => {
+                renderAnswerWrong
+            }, 500);
+        }
     });
 
     function pickQuestion() {
         for (var key in javascriptTrivia) {
-            if (javascriptTrivia[key].type === "checkbox") {
-                renderCheckBox(javascriptTrivia[key]);
+            console.log(key);
+            if (javascriptTrivia[key].type === "t/f") {
+                renderTF(javascriptTrivia[key]);
             } else if (javascriptTrivia[key].type === "radio") {
                 renderRadioButton(javascriptTrivia[key]);
             }
+
         }
     }
 });
